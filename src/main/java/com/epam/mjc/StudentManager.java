42c07eb079152e48242eb864dfb0d1cda018b18e
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-19 13:48:17 +0500

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,9 +6,8 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) throws StudentException{
-    
-    if (studentID > 11 || studentID < 1) {
-            throw new StudentException("Could not find student with ID" + studentID);
+    if (studentID < 1 || studentID > 11) {
+            throw new StudentException("Could not find student with ID " + studentID);
         }
         return Student.getValueOf(studentID);
   }
