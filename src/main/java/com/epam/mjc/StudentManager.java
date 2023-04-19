6a45fc7d8aafce63241d6bc5c4dde34a935bcb92
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-19 13:40:19 +0500

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,30 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentException{
+    
+    if (studentID > 11 || studentID < 1) {
+            throw new StudentManagerException("There was no student with this id " + studentID);
+        }
+        return Student.getValueOf(studentID);
   }
 
+  
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+            try {
+                Student student = manager.find(IDs[i]);
+                System.out.println("Student name " + student.getName());
+            } catch (StudentManagerException exception) {
+                System.out.println(exception);
+            }
+        }
+  }  
+}
+class StudentException extends IllegalArgumentException{
+  public StudentException(String message) {
+        super(message);
     }
-
-  }
 }
